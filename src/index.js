49f72131bc79477c4b946962dc5863470d99b384
Kingsley Asifor <kingsleyasifor@gmail.com>
2022-01-30 23:43:45 +0100

    updated board component with constructor

--- src/index.js
+++ src/index.js
@@ -23,34 +23,41 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
 
-  render() {
+    renderSquare(i) {
+    return <Square value={i} />;
+    }
+
+    render() {
     const status = 'Next player: X';
 
     return (
-      <div>
+        <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
         </div>
-      </div>
+        </div>
     );
-  }
+    }
 }
 
 class Game extends React.Component {
