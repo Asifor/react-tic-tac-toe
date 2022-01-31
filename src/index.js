b021f92c96918805c62e561398fd878aa453527d
Kingsley Asifor <kingsleyasifor@gmail.com>
2022-01-31 04:34:33 +0100

    changed Square class to Square function

--- src/index.js
+++ src/index.js
@@ -2,24 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
+// class Square extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             value: null,
+//         };
+//     }
 
-    render() {
+//     render() {
+//     return (
+//         <button
+//             className="square"
+//             onClick={() => this.props.onClick()}
+//         >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {
     return (
-        <button
-            className="square"
-            onClick={() => this.props.onClick()}
-        >
-        {this.props.value}
-      </button>
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
     );
-  }
 }
 
 class Board extends React.Component {
